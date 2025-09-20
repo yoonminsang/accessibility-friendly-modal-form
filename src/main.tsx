37e8abb0300c55e08094e486379b6e34ea85ffbe
yoonminsang <apdlyooapdl@gmail.com>
2025-09-20 14:10:31 +0900

    feat: overlay provider 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import "modern-normalize";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ModalFormPage from "./ModalFormPage";
+import { OverlayProvider } from "overlay-kit";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ModalFormPage />
+    <OverlayProvider>
+      <ModalFormPage />
+    </OverlayProvider>
   </StrictMode>
 );
